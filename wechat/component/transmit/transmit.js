53f6efef2e6741e4fabf2b6772f854d5938d473e
Mr-taojin <312090373@qq.com>
2020-04-09 23:44:57 +0800

    111

---
+++ wechat/component/transmit/transmit.js
@@ -0,0 +1,27 @@
+// component/transmit /transmit.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  behaviors: [],
+  option:{
+    multipleSlots:true
+  },
+  properties: {
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    popup: '', //打开弹窗类型
+    show_popup: true,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
