d59a97c3644da3c005a264fbcb90ba6a62579ad2
Mr-taojin <312090373@qq.com>
2020-04-23 19:52:50 +0800

    域名更改

--- wechat/utils/config.js
+++ wechat/utils/config.js
@@ -1,6 +1,6 @@
 let apiConfig = {
-  api_url: "https://lycz.lycf888.com", //接口地址
-  upload_path: 'https://lycz.lycf888.com', //后台上传图片地址
+  api_url: "https://api.lycf555.club", //接口地址
+  upload_path: 'https://api.lycf555.club', //后台上传图片地址
   download_path: 'http://jd.lycf388.club', //后台下载图片地址
 }
 export default apiConfig
