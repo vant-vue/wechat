1d54650088f14028fb326de4654a2d0ef5fe9539
jin.tao <312090373@qq.com>
2020-09-02 10:06:27 +0800

    身份认证

--- wechat/pages/subPackagesC/withdraw_deposit/withdraw_deposit.js
+++ wechat/pages/subPackagesC/withdraw_deposit/withdraw_deposit.js
@@ -1,6 +1,8 @@
 // pages/subPackagesC/withdraw_deposit/withdraw_deposit.js
 const app = getApp();
 import config from '../../../utils/config.js'
+const default_url_one = "/images/idCard/idNumFront.png"
+const default_url_two = "/images/idCard/idNumBack.png"
 Page({
 
   /**
@@ -8,10 +10,14 @@ Page({
    */
   data: {
     isPayee: '',
-    isIdNum:'',
+    isIdNum: '',
     cashAmount: '',
-    is_request:false,
-    userInfo: {}
+    is_request: false,
+    userInfo: {},
+    default_url_one: default_url_one,
+    default_url_two: default_url_two,
+    uploaderList: [],
+    isIdCard: false
   },
   //监听input 普通输入框
   listenerInputSimple(e) {
@@ -24,8 +30,8 @@ Page({
       [row]: value.replace(/,/g, "")
     });
   },
-   //监听input 普通输入框
-   listenerInputSimpleIdNum(e) {
+  //监听input 普通输入框
+  listenerInputSimpleIdNum(e) {
     let value = e.detail.value || '';
     let row = e.currentTarget.dataset.row;
     if (value.length > 18) {
@@ -35,46 +41,82 @@ Page({
       [row]: value.replace(/,/g, "")
     });
   },
-  idNumCheck(code){
+  idNumCheck(code) {
     var ts = this;
 
     //身份证号合法性验证 
     //支持15位和18位身份证号
     //支持地址编码、出生日期、校验位验证
-      var city = { 11: "北京", 12: "天津", 13: "河北", 14: "山西", 15: "内蒙古", 21: "辽宁", 22: "吉林", 23: "黑龙江 ", 31: "上海", 32: "江苏", 33: "浙江", 34: "安徽", 35: "福建", 36: "江西", 37: "山东", 41: "河南", 42: "湖北 ", 43: "湖南", 44: "广东", 45: "广西", 46: "海南", 50: "重庆", 51: "四川", 52: "贵州", 53: "云南", 54: "西藏 ", 61: "陕西", 62: "甘肃", 63: "青海", 64: "宁夏", 65: "新疆", 71: "台湾", 81: "香港", 82: "澳门", 91: "国外 " };
-      var tip = "";
-      var pass = true;
+    var city = {
+      11: "北京",
+      12: "天津",
+      13: "河北",
+      14: "山西",
+      15: "内蒙古",
+      21: "辽宁",
+      22: "吉林",
+      23: "黑龙江 ",
+      31: "上海",
+      32: "江苏",
+      33: "浙江",
+      34: "安徽",
+      35: "福建",
+      36: "江西",
+      37: "山东",
+      41: "河南",
+      42: "湖北 ",
+      43: "湖南",
+      44: "广东",
+      45: "广西",
+      46: "海南",
+      50: "重庆",
+      51: "四川",
+      52: "贵州",
+      53: "云南",
+      54: "西藏 ",
+      61: "陕西",
+      62: "甘肃",
+      63: "青海",
+      64: "宁夏",
+      65: "新疆",
+      71: "台湾",
+      81: "香港",
+      82: "澳门",
+      91: "国外 "
+    };
+    var tip = "";
+    var pass = true;
     var reg = /^\d{6}(18|19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/;
     if (!code || !code.match(reg)) {
-        tip = "身份证号格式错误";
-        pass = false;
-      }else if (!city[code.substr(0, 2)]) {
-        tip = "地址编码错误";
-        pass = false;
-      }else {
-        //18位身份证需要验证最后一位校验位
-        if (code.length == 18) {
-          code = code.split('');
-          //∑(ai×Wi)(mod 11)
-          //加权因子
-          var factor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
-          //校验位
-          var parity = [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2];
-          var sum = 0;
-          var ai = 0;
-          var wi = 0;
-          for (var i = 0; i < 17; i++) {
-            ai = code[i];
-            wi = factor[i];
-            sum += ai * wi;
-          }
-          var last = parity[sum % 11];
-          if (parity[sum % 11] != code[17]) {
-            tip = "校验位错误";
-            pass = false;
-          }
+      tip = "身份证号格式错误";
+      pass = false;
+    } else if (!city[code.substr(0, 2)]) {
+      tip = "地址编码错误";
+      pass = false;
+    } else {
+      //18位身份证需要验证最后一位校验位
+      if (code.length == 18) {
+        code = code.split('');
+        //∑(ai×Wi)(mod 11)
+        //加权因子
+        var factor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+        //校验位
+        var parity = [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2];
+        var sum = 0;
+        var ai = 0;
+        var wi = 0;
+        for (var i = 0; i < 17; i++) {
+          ai = code[i];
+          wi = factor[i];
+          sum += ai * wi;
+        }
+        var last = parity[sum % 11];
+        if (parity[sum % 11] != code[17]) {
+          tip = "校验位错误";
+          pass = false;
         }
       }
+    }
     return pass;
   },
   //监听input 普通输入框
@@ -97,14 +139,14 @@ Page({
       'cashAmount': this.data.userInfo.cashAmount / 100
     })
   },
-  getPhoneNumber(e){
+  getPhoneNumber(e) {
     var that = this;
     wx.checkSession({
-      success () {
+      success() {
         let params = {
           param: {
-            "iv":e.detail.iv,
-            "encryptedData":e.detail.encryptedData
+            "iv": e.detail.iv,
+            "encryptedData": e.detail.encryptedData
           }
         };
         app.$API.getWxRzPhone(params).then(res => {
@@ -117,14 +159,14 @@ Page({
           }
         })
       },
-      fail () {
+      fail() {
         // session_key 已经失效，需要重新执行登录流程
         wx.navigateTo({
           url: 'pages/tabBar/login/login'
         })
       }
     })
-      
+
   },
   /**
    * 生命周期函数--监听页面加载
@@ -139,13 +181,119 @@ Page({
         this.setData({
           userInfo: res.args,
           isPayee: res.args.payee ? true : false,
-          isIdNum:res.args.idNumber ? true:false,
+          isIdNum: res.args.idNumber ? true : false,
           cashAmount: ''
         });
       }
     })
-  },//微信认证信息提交
-  wxRz(){
+  },
+  // 选择图片
+  upload: function(e) {
+    console.log(e);
+    var that = this;
+    wx.chooseImage({
+      count: that.data.defaultNum - that.data.uploaderNum, // 默认数量
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function(res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        let tempFilePaths = res.tempFilePaths;
+        let uploaderList = that.data.uploaderList.concat(tempFilePaths);
+        if (uploaderList.length == that.data.defaultNum) {
+          that.setData({
+            showUpload: false
+          })
+        }
+        that.setData({
+          uploaderList: uploaderList,
+          uploaderNum: uploaderList.length,
+        })
+        that.triggerEvent('upload', that.data.uploaderList);
+        that.get_img_list(that.data.uploaderList, e.currentTarget.dataset.type);
+      }
+    })
+  },
+  // 图片上传
+  uploadImg(imgurl) {
+    console.log(imgurl, 'imgurl');
+    return new Promise((resolve, reject) => {
+      if (imgurl.length == 0) {
+        resolve([]);
+        return;
+      };
+      this.data.pictureList = [];
+      let that = this;
+      let token = wx.getStorageSync('token') || "";
+      let header = {
+        'content-type': 'multipart/form-data'
+      }
+      if (token) {
+        header.Authorization = token;
+      };
+      for (let i = 0; i < imgurl.length; i++) {
+        if (imgurl[i].indexOf(config.download_path) > -1) {
+          that.data.pictureList.push(imgurl[i]);
+          if (that.data.pictureList.length == imgurl.length) {
+            resolve(that.data.pictureList);
+          }
+          continue;
+        }
+        wx.uploadFile({
+          url: config.api_url + "/solitaire/uploadFiles/uploadImg",
+          filePath: imgurl[i],
+          name: 'file',
+          header: header,
+          formData: {
+            dir: 'solitaire'
+          },
+          success: function(res) {
+            let result = JSON.parse(res.data);
+            that.data.pictureList.push(result.args.path);
+            if (that.data.pictureList.length == imgurl.length) {
+              resolve(that.data.pictureList);
+            }
+          },
+          fail: function(res) {
+            wx.showModal({
+              title: '错误提示',
+              content: '上传图片失败',
+              showCancel: false,
+              success: function(res) {}
+            });
+            reject(res);
+          },
+          complete: (() => {
+
+          })
+        })
+      }
+    })
+  },
+  get_img_list(list, type) {
+    wx.showLoading({
+      title: '上传中',
+      mask: true
+    })
+    this.uploadImg(list).then(res => {
+      this.data.uploaderList = [];
+      wx.hideLoading();
+      let row = "";
+      if (type == "idNumFront") {
+        row = "default_url_one"
+      } else if (type == "idNumBack") {
+        row = "default_url_two"
+      }
+      console.log(row,type);
+      this.setData({
+        [row]: res.join('')
+      })
+    });
+  },
+  //微信认证信息提交
+  wxRz() {
+    this.setData({
+      isIdCard:true
+    })
     if (this.data.is_request) {
       return;
     }
@@ -169,25 +317,25 @@ Page({
       this.data.is_request = false;
       return;
     }
-    if(!this.data.userInfo.idNumber){
-      wx.showToast({
-        title: '请输入收款人身份证号码',
-        icon: 'none',
-        duration: config.timeoutSecond
-      })
-      this.data.is_request = false;
-      return;
-    }
-    if(!this.idNumCheck(this.data.userInfo.idNumber)){
-      wx.showToast({
-        title: '收款人身份证号码错误',
-        icon: 'none',
-        duration: config.timeoutSecond
-      })
-      this.data.is_request = false;
-      return;
-    }
-    if(!this.data.userInfo.idNumFront){
+    // if(!this.data.userInfo.idNumber){
+    //   wx.showToast({
+    //     title: '请输入收款人身份证号码',
+    //     icon: 'none',
+    //     duration: config.timeoutSecond
+    //   })
+    //   this.data.is_request = false;
+    //   return;
+    // }
+    // if(!this.idNumCheck(this.data.userInfo.idNumber)){
+    //   wx.showToast({
+    //     title: '收款人身份证号码错误',
+    //     icon: 'none',
+    //     duration: config.timeoutSecond
+    //   })
+    //   this.data.is_request = false;
+    //   return;
+    // }
+    if (!this.data.userInfo.idNumFront) {
       wx.showToast({
         title: '请上传微信正面图片',
         icon: 'none',
@@ -196,7 +344,7 @@ Page({
       this.data.is_request = false;
       return;
     }
-    if(!this.data.userInfo.idNumBack){
+    if (!this.data.userInfo.idNumBack) {
       wx.showToast({
         title: '请上传微信反面图片',
         icon: 'none',
@@ -209,9 +357,9 @@ Page({
       param: {
         "idNumber": this.data.idNumber, //身份证号码
         "name": this.data.userInfo.payee, //实名  可空已经实名过一次的 可以为空
-        "rzPhone":this.data.userInfo.rzPhone,//微信授权手机号认证
-        "idNumFront":this.data.userInfo.idNumFront,//身份证正面照片
-        "idNumBack":this.data.userInfo.idNumBack //身份证反面照片
+        "rzPhone": this.data.userInfo.rzPhone, //微信授权手机号认证
+        "idNumFront": this.data.userInfo.idNumFront, //身份证正面照片
+        "idNumBack": this.data.userInfo.idNumBack //身份证反面照片
       }
     }
     wx.showLoading({
@@ -295,7 +443,10 @@ Page({
     })
   },
   cancel() {
-    wx.navigateBack();
+    this.setData({
+      isIdCard:false
+    })
+    // wx.navigateBack();
   },
   //问题
   /**
